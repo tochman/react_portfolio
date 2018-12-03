d0e32b5382a6510a81b91ee519e2342054fd96e3
Thomas <thomas@craftacademy.se>
2018-12-03 15:49:58 +0100

    adds code snippets for header and footer

---
+++ src/Footer.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react"
+
+class Footer extends Component {
+
+    render() {
+        return (
+            <h1>Made with React</h1>
+        )
+    }
+}
+
+export default Footer
--- src/Header.jsx
+++ src/Header.jsx
@@ -4,8 +4,8 @@ class Header extends Component {
 
     render() {
         return (
-            <nav className="flex items-center justify-between flex-wrap bg-blue-darkest p-6">
-                <h1 className="text-white text-3xl">My Portfolio</h1>
+            <nav>
+                <h1>My Portfolio</h1>
             </nav>
         )
     }
