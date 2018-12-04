88aaefa7d319d51b40106fe25cc6d5e77ac55df8
Thomas <thomas@craftacademy.se>
2018-12-04 07:49:41 +0100

    fixes problem with double reference of content-wrapper. styling is a bitch... :zap:

--- src/About.jsx
+++ src/About.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 const About = () => (
-    <div className="content-wrapper">
+    <div>
         <h1 className="content-text">About Me</h1>
         <p className="content-text">Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
     </div>
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 const Projects = () => (
-    <div className="content-wrapper">
+    <div>
         <h1 className="content-text">My Projects</h1>
         <p className="content-text">Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
     </div>
