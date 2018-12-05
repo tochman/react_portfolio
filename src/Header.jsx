3964f162a61ea9b334eced13259f41b7e1c58881
Thomas <thomas@craftacademy.se>
2018-12-05 05:54:16 +0100

    refactors links and cleans some code

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,18 +1,16 @@
 import React from "react"
-import { NavLink, Route, Switch } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <nav className="header">
-
             <h1 className="header-title">
-                <NavLink className="text-white hover:text-grey no-underline" exact to='/'>My Portfolio</NavLink>
+                <Link className="text-white hover:text-grey no-underline" exact to='/'>My Portfolio</Link>
             </h1>
             <ul className="list-reset flex">
-                <li className="mr-6"><NavLink className="text-white hover:text-grey no-underline" exact to='/about'>About Me</NavLink></li>
-                <li className="mr-6"><NavLink className="text-white hover:text-grey no-underline" exact to='/projects'>My Projects</NavLink></li>
+                <li className="mr-6"><NavLink className="text-white hover:text-grey no-underline" activeStyle={{fontWeight: "bold"}} exact to='/about'>About Me</NavLink></li>
+                <li className="mr-6"><NavLink className="text-white hover:text-grey no-underline" activeStyle={{fontWeight: "bold"}} exact to='/projects'>My Projects</NavLink></li>
             </ul>
-
         </nav>
     )
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { BrowserRouter } from 'react-router-dom';
-import { Switch, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 
 import './css/tailwind.css';
 import Hello from "./Hello"
