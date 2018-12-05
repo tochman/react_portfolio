0da122f71987df654d40920712e7b71397b79d00
Thomas <thomas@craftacademy.se>
2018-12-05 07:42:35 +0100

    adds projets in json format and displays them on the projects path

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,11 +5,11 @@ const Header = () => {
     return (
         <nav className="header">
             <h1 className="header-title">
-                <Link className="text-white hover:text-grey no-underline" exact to='/'>My Portfolio</Link>
+                <Link className="text-white hover:text-grey no-underline" to='/'>My Portfolio</Link>
             </h1>
             <ul className="list-reset flex">
-                <li className="mr-6"><NavLink className="text-white hover:text-grey no-underline" activeStyle={{fontWeight: "bold"}} exact to='/about'>About Me</NavLink></li>
-                <li className="mr-6"><NavLink className="text-white hover:text-grey no-underline" activeStyle={{fontWeight: "bold"}} exact to='/projects'>My Projects</NavLink></li>
+                <li className="mr-6"><NavLink className="text-white hover:text-grey no-underline" activeStyle={{fontWeight: "bold"}} to='/about'>About Me</NavLink></li>
+                <li className="mr-6"><NavLink className="text-white hover:text-grey no-underline" activeStyle={{fontWeight: "bold"}} to='/projects'>My Projects</NavLink></li>
             </ul>
         </nav>
     )
