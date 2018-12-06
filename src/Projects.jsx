9d261840cdc55f2078e0412e0fca4cdcbd7da02a
Thomas <thomas@craftacademy.se>
2018-12-06 06:34:27 +0100

    adds project cards and more attributes

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -22,15 +22,37 @@ class Projects extends Component {
     }
     render() {
         const projects = this.state.projects
-        let projectsList 
+        let projectsList
 
         if (projects.length > 0) {
             projectsList = projects.map(project => {
                 return (
-                    <div key={project.id}>
-                        <h3 className="content-text">
-                            {project.name}
-                        </h3>
+                    // wrapper
+
+                    <div key={project.id} className="m-2 leading-normal flex">
+                        {/* card content */}
+
+                        <div className="max-w-lg w-full border rounded overflow-hidden m-4 sm:flex">
+                            {/* card image */}
+
+                            <div
+                                className="h-48 sm:h-auto sm:w-48 md:w-64 flex-none bg-cover bg-center text-center overflow-hidden"
+                                style={{ backgroundImage: `url('${project.image}')` }}>
+                            </div>
+                            <div className="px-6 py-4">
+                                <h2 className="mb-2 font-black">{project.name}</h2>
+                                <p className="mb-4 text-grey-dark text-sm">
+                                    {project.description}
+                                </p>
+
+                                {/* button */}
+                                <button className="py-3 px-6 bg-purple hover:bg-purple-light text-white font-bold rounded-full mt-1 mb-2">
+                                    Details
+                                </button>
+
+                            </div>
+
+                        </div>
                     </div>
                 )
             })
@@ -41,9 +63,10 @@ class Projects extends Component {
             <div>
                 <h1 className="content-text">My Projects</h1>
                 {projectsList}
-            </div>
+            </div >
         )
     }
 };
 
 export default Projects
+
